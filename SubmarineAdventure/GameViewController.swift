00d65436d380d78e19a140655b9e4e922b7fd1d3
sapaGit <mosevaly@gmail.com>
2022-06-02 10:53:54 +0300

    01.2 - Add ViewControllers

---
+++ SubmarineAdventure/GameViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GameViewController.swift
+//  SubmarineAdventure
+//
+//  Created by Sergey Pavlov on 02.06.2022.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ SubmarineAdventure/RecordsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecordsViewController.swift
+//  SubmarineAdventure
+//
+//  Created by Sergey Pavlov on 02.06.2022.
+//
+
+import UIKit
+
+class RecordsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
