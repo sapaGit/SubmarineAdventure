b9ee4774ffbbebd38b6829e15a111a5bb38cb82b
sapaGit <mosevaly@gmail.com>
2022-06-02 10:32:17 +0300

    Initial Commit

---
+++ SubmarineAdventure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubmarineAdventure
+//
+//  Created by Sergey Pavlov on 02.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
